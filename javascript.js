a8c073983da1f343ce667760ef6c86eeac68da50
Mishiez <michellegathonimain@gmail.com>
2024-07-10 09:02:55 +0300

    Added a function to remove the new grid and create a new one bassed on the input the user enters after pressing the button

--- javascript.js
+++ javascript.js
@@ -1,14 +1,47 @@
 for(let i=0;i<256;i++){
-    const container=document.querySelector("#container");
-    const box=document.createElement("div");
-    box.classList.add("box");
-    box.textContent="PO";//will be remved in future
-    container.appendChild(box);
+  const container=document.querySelector("#container");
+  const box=document.createElement("div");
+  box.classList.add("box");
+  container.appendChild(box);
 }
 
-const boxes=document.querySelectorAll(".box");
+let boxes=document.querySelectorAll(".box");
 boxes.forEach(box=>{
-  box.addEventListener('mouseover',function(){
-    this.style.backgroundColor='red';
-  });
+box.addEventListener('mouseover',function(){
+  this.style.backgroundColor='red';
 });
+});
+
+let btn=document.getElementById("btnSquares");
+btn.addEventListener('click', function () {
+  let boxes = document.querySelectorAll('.box');
+  boxes.forEach(box => {
+    box.remove();//removes old grid
+  });
+  createGrid() //makes new grid afte reset
+})
+
+
+function createGrid() {
+  const Container = document.getElementById("container");
+    Container.innerHTML = '';
+  let num=prompt("Enter no.os squares: ");
+  if(num<=100){
+  for (let i = 0; i < (num*num); i++) {
+      const grid = document.createElement('div');
+      let a = num;
+     const boxSize = 32 / a; // creates the dynamic squeres
+      grid.classList.add('grid');
+      grid.style.width = boxSize + 'rem';
+      grid.style.height = boxSize + 'rem';
+     Container.appendChild(grid);
+     grid.addEventListener("mouseenter", function () {
+      this.style.backgroundColor = "red";
+     });
+  }
+}
+else{
+  alert("The number should not exceed 100");
+}
+
+}
